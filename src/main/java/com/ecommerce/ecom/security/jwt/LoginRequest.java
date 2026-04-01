3a3c46d5e6995ab2513797ed12800f4429f5a0f1
Aditya Gaur <adityaofficial707@gmail.com>
2026-04-01 22:25:41 +0530

    Added JWT related files to work with Spring Security

---
+++ src/main/java/com/ecommerce/ecom/security/jwt/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecom.security.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LoginRequest {
+    private String username;
+
+    private String password;
+
+}
