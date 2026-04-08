e5124ea8924f8fd2623ea0bfe7d83680654f1ebc
Aditya Gaur <adityaofficial707@gmail.com>
2026-04-08 21:07:02 +0530

    Added DTOs for Cart and CartItem

---
+++ src/main/java/com/ecommerce/ecom/payload/CartItemDTO.java
@@ -0,0 +1,17 @@
+package com.ecommerce.ecom.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItemDTO {
+    private Long cartItemId;
+    private CartDTO cart;
+    private ProductDTO productDTO;
+    private Integer quantity;
+    private Double discount;
+    private Double productPrice;
+}
