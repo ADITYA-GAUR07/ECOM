ab2678e721443896b1e8fdad514d820f7b754fa1
Aditya Gaur <adityaofficial707@gmail.com>
2026-04-18 18:54:17 +0530

    Added api to change quantity of product in cart

--- src/main/java/com/ecommerce/ecom/repository/CartItemRepository.java
+++ src/main/java/com/ecommerce/ecom/repository/CartItemRepository.java
@@ -8,4 +8,5 @@ public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     @Query("SELECT ci FROM CartItem ci WHERE ci.cart.id = ?1 AND ci.product.id = ?2")
     CartItem findCartItemByPorductIdAndCartId(Long cartId, Long productId);
+
 }
